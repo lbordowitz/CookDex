fb3fc5248d4f6bf30c82f520f5eb29c7e2e81851
Larry Bordowitz <larry@gomerits.com>
2020-11-13 22:29:55 -0800

    Put raw ingredients in Shepherds Pie

--- shepherds_pie.go
+++ shepherds_pie.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("Shepherd's pie is delicious, here is how to make it.")
+	potatoes := RawIngredient{Name: "Potatoes"}
+	butter := RawIngredient{Name: "Butter"}
+	onions := RawIngredient{Name: "Onions"}
+	veggies := RawIngredient{Name: "Veggies"}
+	rawBeef := RawIngredient{Name: "Raw Ground Beef"}
+	beefBroth := RawIngredient{Name: "Beef Broth"}
 }
