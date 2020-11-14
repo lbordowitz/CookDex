5182fdddf3dda36003601f21ca087308d4da8afa
Larry Bordowitz <larry@gomerits.com>
2020-11-13 22:44:10 -0800

    Mechanically prepared ingredient, and an example of a peeled potato

--- ingredients.go
+++ ingredients.go
@@ -1,7 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Ingredient interface {
 	Display() string
+	IngredientReferenceName() string
 	Components() []Ingredient
 	Time() int
 }
@@ -14,10 +20,43 @@ func (ingredient *RawIngredient) Display() string {
 	return ingredient.Name
 }
 
+func (ingredient *RawIngredient) IngredientReferenceName() string {
+	return ingredient.Name
+}
+
 func (ingredient *RawIngredient) Components() []Ingredient {
 	return nil
 }
 
 func (ingredient *RawIngredient) Time() int {
 	return 0
+}
+
+type MechanicallyPreparedIngredient struct {
+	Technique  string
+	Name       string
+	components []Ingredient
+	time       int
+}
+
+func (ingredient *MechanicallyPreparedIngredient) Display() string {
+
+	componentIngredientNames := make([]string, len(ingredient.components))
+	for i, v := range ingredient.components {
+		componentIngredientNames[i] = v.IngredientReferenceName()
+	}
+
+	return fmt.Sprintf("{} the {}", ingredient.Technique, strings.Join(componentIngredientNames, ", "))
+}
+
+func (ingredient *MechanicallyPreparedIngredient) IngredientReferenceName() string {
+	return ingredient.Name
+}
+
+func (ingredient *MechanicallyPreparedIngredient) Components() []Ingredient {
+	return ingredient.components
+}
+
+func (ingredient *MechanicallyPreparedIngredient) Time() int {
+	return ingredient.time
 }
--- shepherds_pie.go
+++ shepherds_pie.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Shepherd's pie is delicious, here is how to make it.")
 	potatoes := RawIngredient{Name: "Potatoes"}
+	peeledPotatoes := MechanicallyPreparedIngredient{Technique: "Peel", Name: "Peeled Potatoes", components: []Ingredient{&potatoes}, time: 2}
 	butter := RawIngredient{Name: "Butter"}
 	onions := RawIngredient{Name: "Onions"}
 	veggies := RawIngredient{Name: "Veggies"}
