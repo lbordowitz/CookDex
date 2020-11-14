d53f19969c165f1cad0adf7c86b1360d340d1cd6
Larry Bordowitz <larry@gomerits.com>
2020-11-13 22:51:31 -0800

    A few more mechanical ingredients, but now we need heat.

--- shepherds_pie.go
+++ shepherds_pie.go
@@ -6,9 +6,18 @@ func main() {
 	fmt.Println("Shepherd's pie is delicious, here is how to make it.")
 	potatoes := RawIngredient{Name: "Potatoes"}
 	peeledPotatoes := MechanicallyPreparedIngredient{Technique: "Peel", Name: "Peeled Potatoes", components: []Ingredient{&potatoes}, time: 2}
+	quarteredPotatoes := MechanicallyPreparedIngredient{Technique: "Quarter", Name: "Quartered Potatoes", components: []Ingredient{&peeledPotatoes}, time: 2}
+	// boil potatoes
 	butter := RawIngredient{Name: "Butter"}
+	// mash potatoes, with butter
 	onions := RawIngredient{Name: "Onions"}
+	choppedOnions := MechanicallyPreparedIngredient{Technique: "Chop", Name: "chopped Onions", components: []Ingredient{&onions}, time: 2}
+	// saute onions
 	veggies := RawIngredient{Name: "Veggies"}
+	// cook veggies and onions
 	rawBeef := RawIngredient{Name: "Raw Ground Beef"}
+	// cook raw beef vegetables
 	beefBroth := RawIngredient{Name: "Beef Broth"}
+	// layer shepherds pie
+	// bake shepherds pie
 }
