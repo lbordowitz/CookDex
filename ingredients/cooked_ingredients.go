5e7bb850ddd0dcf8887ed8017124ffea8c7b8273
Larry Bordowitz <larry@gomerits.com>
2020-11-14 00:08:01 -0800

    Cooked ingredients, first draft

---
+++ ingredients/cooked_ingredients.go
@@ -0,0 +1,58 @@
+package ingredients
+
+import (
+	"fmt"
+	"strings"
+)
+
+type CookedIngredient struct {
+	Technique                                           string
+	Name                                                string
+	components                                          []Ingredient
+	time                                                int
+	heat                                                string
+	indexOfIngredientInWhichTheRestOfTheIngredientsCook int
+}
+
+func CookingTechniqueInStuff(Technique string, ResultingIngredient string, SubIngredients []Ingredient, Time int, Heat string, In int) *CookedIngredient {
+	return &CookedIngredient{
+		Technique:  Technique,
+		Name:       ResultingIngredient,
+		components: SubIngredients,
+		time:       Time,
+		heat:       Heat,
+		indexOfIngredientInWhichTheRestOfTheIngredientsCook: In,
+	}
+}
+func CookingTechnique(Technique string, ResultingIngredient string, SubIngredients []Ingredient, Time int, Heat string) *CookedIngredient {
+	return &CookedIngredient{
+		Technique:  Technique,
+		Name:       ResultingIngredient,
+		components: SubIngredients,
+		time:       Time,
+		heat:       Heat,
+		indexOfIngredientInWhichTheRestOfTheIngredientsCook: -1,
+	}
+}
+func (ingredient *CookedIngredient) Display() string {
+
+	componentIngredientNames := make([]string, len(ingredient.components))
+	for i, v := range ingredient.components {
+		componentIngredientNames[i] = v.IngredientReferenceName()
+	}
+	// TODO exclude "in" ingredient, add it later as "in x" or something.
+
+	return fmt.Sprintf("%s the %s, at %s heat, for %d minutes", ingredient.Technique, strings.Join(componentIngredientNames, ", "), ingredient.heat, ingredient.time)
+}
+
+func (ingredient *CookedIngredient) IngredientReferenceName() string {
+	return ingredient.Name
+}
+
+func (ingredient *CookedIngredient) Components() []Ingredient {
+	return ingredient.components
+}
+
+func (ingredient *CookedIngredient) Time() int {
+	return ingredient.time
+}
